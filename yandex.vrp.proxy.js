02e11b813745be01f646695eec43e9bfece953d4
bruto8000 <54044706+bruto8000@users.noreply.github.com>
2021-07-06 14:59:51 +0300

    Changing domain:

--- yandex.vrp.proxy.js
+++ yandex.vrp.proxy.js
@@ -1,43 +1,43 @@
 const open = require("open");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// app.get(
-//   "/.well-known/acme-challenge/180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs",
-//   (req, res) => {
-//     res.end(
-//       "180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE"
-//     );
-//   }
-// );
-// app.listen(80, () => {
-//   console.log("WORKS");
-// });
+const app = express();
+app.get(
+  "/.well-known/acme-challenge/GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc",
+  (req, res) => {
+    res.end(
+      "GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE"
+    );
+  }
+);
+app.listen(80, () => {
+  console.log("WORKS");
+});
 
-require("dotenv").config();
-let port = 8080;
+// require("dotenv").config();
+// let port = 8080;
 
-let cors_proxy = require("cors-anywhere");
-cors_proxy
-  .createServer({
-    httpsOptions: {
-      key: fs.readFileSync(
-        "/etc/letsencrypt/live/kupitbitovku.ru/privkey.pem",
-        "utf8"
-      ),
-      cert: fs.readFileSync(
-        "/etc/letsencrypt/live/kupitbitovku.ru/cert.pem",
-        "utf8"
-      ),
-    },
-    originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
-  })
-  .listen(port, function (a, b) {
-    console.log(`
-    Все работает.
-  `);
-  });
+// let cors_proxy = require("cors-anywhere");
+// cors_proxy
+//   .createServer({
+//     httpsOptions: {
+//       key: fs.readFileSync(
+//         "/etc/letsencrypt/live/kupitbitovku.ru/privkey.pem",
+//         "utf8"
+//       ),
+//       cert: fs.readFileSync(
+//         "/etc/letsencrypt/live/kupitbitovku.ru/cert.pem",
+//         "utf8"
+//       ),
+//     },
+//     originWhitelist: [], // Allow all origins
+//     requireHeader: ["origin", "x-requested-with"],
+//     removeHeaders: ["cookie", "cookie2"],
+//   })
+//   .listen(port, function (a, b) {
+//     console.log(`
+//     Все работает.
+//   `);
+//   });
