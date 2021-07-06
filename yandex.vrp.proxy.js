2689a174f297572044f40db4334b796ae6b5fc2e
bruto8000 <54044706+bruto8000@users.noreply.github.com>
2021-07-06 15:02:09 +0300

    TURN ON PROXY

--- yandex.vrp.proxy.js
+++ yandex.vrp.proxy.js
@@ -3,41 +3,41 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const express = require("express");
 
-const app = express();
-app.get(
-  "/.well-known/acme-challenge/GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc",
-  (req, res) => {
-    res.end(
-      "GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE"
-    );
-  }
-);
-app.listen(80, () => {
-  console.log("WORKS");
-});
+// const app = express();
+// app.get(
+//   "/.well-known/acme-challenge/GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc",
+//   (req, res) => {
+//     res.end(
+//       "GFXD8P2_YbrfZP0r3GrL9ma8036WHAdSQY_f41e0qGc.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE"
+//     );
+//   }
+// );
+// app.listen(80, () => {
+//   console.log("WORKS");
+// });
 
-// require("dotenv").config();
-// let port = 8080;
+require("dotenv").config();
+let port = 8080;
 
-// let cors_proxy = require("cors-anywhere");
-// cors_proxy
-//   .createServer({
-//     httpsOptions: {
-//       key: fs.readFileSync(
-//         "/etc/letsencrypt/live/kupitbitovku.ru/privkey.pem",
-//         "utf8"
-//       ),
-//       cert: fs.readFileSync(
-//         "/etc/letsencrypt/live/kupitbitovku.ru/cert.pem",
-//         "utf8"
-//       ),
-//     },
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: ["origin", "x-requested-with"],
-//     removeHeaders: ["cookie", "cookie2"],
-//   })
-//   .listen(port, function (a, b) {
-//     console.log(`
-//     Все работает.
-//   `);
-//   });
+let cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    httpsOptions: {
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/beeserver.ru/privkey.pem",
+        "utf8"
+      ),
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/beeserver.ru/cert.pem",
+        "utf8"
+      ),
+    },
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, function (a, b) {
+    console.log(`
+    Все работает.
+  `);
+  });
