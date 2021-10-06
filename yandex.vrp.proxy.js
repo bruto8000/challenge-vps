6250294d88429d7e8c97c6ed06b37972a196e1b0
bruto8000 <54044706+bruto8000@users.noreply.github.com>
2021-10-06 06:15:39 +0300

    Включил сервер на 80м порту

--- yandex.vrp.proxy.js
+++ yandex.vrp.proxy.js
@@ -1,20 +1,20 @@
 const open = require("open");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
-// app.get(
-//   "/.well-known/acme-challenge/180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs",
-//   (req, res) => {
-//     res.end(
-//       "180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE"
-//     );
-//   }
-// );
-// app.listen(80, () => {
-//   console.log("WORKS");
-// });
+const app = express();
+app.get(
+  "/.well-known/acme-challenge/180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs",
+  (req, res) => {
+    res.end(
+      "180W3EfHIaPF1NH4cb4WmqE2uNUGJzSK3N5BFgXABAs.ima0xUnIkqcIsny9uiHKW1jvL3thEZXTyIvO5CeeJiE",
+    );
+  },
+);
+app.listen(80, () => {
+  console.log("WORKS");
+});
 
 require("dotenv").config();
 let port = 8080;
@@ -25,11 +25,11 @@ cors_proxy
     httpsOptions: {
       key: fs.readFileSync(
         "/etc/letsencrypt/live/kupitbitovku.ru/privkey.pem",
-        "utf8"
+        "utf8",
       ),
       cert: fs.readFileSync(
         "/etc/letsencrypt/live/kupitbitovku.ru/cert.pem",
-        "utf8"
+        "utf8",
       ),
     },
     originWhitelist: [], // Allow all origins
